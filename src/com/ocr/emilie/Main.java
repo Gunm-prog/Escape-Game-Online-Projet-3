3e5a05bb2340e2bd23e75c9f55887922d793c00e
Emilie Balsen <em.balsen@laposte.net>
2019-11-15 14:37:49 +0100

    Ajout de l'affichage du menu

--- src/com/ocr/emilie/Main.java
+++ src/com/ocr/emilie/Main.java
@@ -1,8 +1,34 @@
 package com.ocr.emilie;
 
+/**
+ *
+ */
 public class Main {
 
+
+    private static String nomUtilisateur = "Saisir un nom d'utilisateur";
+
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Bienvenue");
+        System.out.println("Saisir un nom d'utilisateur");
+        System.out.println("Choix du mode de jeu");
+        System.out.println("1 - Challenger");
+        System.out.println("2 - Défenseur");
+        System.out.println("3 - Duel");
+        System.out.println("4 - Quitter");
+        System.out.println("Veuillez choisir votre mode de jeu");
+        // write your code here
+    }
+
+    public static void setNomUtilisateur(String nomUtilisateur) {
+        Main.nomUtilisateur = "Saisir un nom d'utilisateur";
+    }
+
+    /**
+     * Display a selected Gaming Mode
+     * @param nbGamingMode The Selected Gaming Mode
+     */
+    public void displaySelectedGamingMode (int nbGamingMode){
+        System.out.println("Vous avez choisi le mode de jeu" + nbGamingMode);
     }
 }
