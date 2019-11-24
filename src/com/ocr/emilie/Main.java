0a930a6237ac2a6bc756cff126ae61f5148fb927
Emilie Balsen <em.balsen@laposte.net>
2019-11-24 12:31:56 +0100

    Ajout récupération saisie nom utilisateur et choix du mode de jeu

--- src/com/ocr/emilie/Main.java
+++ src/com/ocr/emilie/Main.java
@@ -1,5 +1,7 @@
 package com.ocr.emilie;
 
+import java.util.Scanner;
+
 /**
  *
  */
@@ -10,25 +12,40 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Bienvenue");
-        System.out.println("Saisir un nom d'utilisateur");
-        System.out.println("Choix du mode de jeu");
+        System.out.println("Choix du mode de jeu : 1 - Challenger");
         System.out.println("1 - Challenger");
         System.out.println("2 - Défenseur");
         System.out.println("3 - Duel");
         System.out.println("4 - Quitter");
-        System.out.println("Veuillez choisir votre mode de jeu");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Saisissez un nom d'utilisateur : emilie");
+        String str = sc.nextLine();
+        System.out.println("Pseudo choisi: " + str);
+        System.out.println("Saisissez un mode de jeu: 1 - Challenger ");
+        int i = sc.nextInt();
+        displaySelectedGamingMode(i);
+
+
+
         // write your code here
-    }
 
-    public static void setNomUtilisateur(String nomUtilisateur) {
-        Main.nomUtilisateur = "Saisir un nom d'utilisateur";
-    }
 
+    }
     /**
      * Display a selected Gaming Mode
      * @param nbGamingMode The Selected Gaming Mode
      */
-    public void displaySelectedGamingMode (int nbGamingMode){
-        System.out.println("Vous avez choisi le mode de jeu" + nbGamingMode);
+    public static void displaySelectedGamingMode(int nbGamingMode) {
+        if (nbGamingMode == 1) {
+            System.out.println("Vous avez choisi le mode de jeu : Challenger");
+        } else if (nbGamingMode == 2) {
+            System.out.println("Vous avez choisi le mode de jeu : Défenseur");
+        } else if (nbGamingMode == 3) {
+            System.out.println("Vous avez choisi le mode de jeu: Duel");
+        } else {
+            System.out.println("Vous n'avez pas choisi de menu parmi les choix proposés");
+        }
     }
+
+
 }
